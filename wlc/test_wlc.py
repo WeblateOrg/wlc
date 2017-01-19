6b2b17aa3fbdc12d45aebb50ebc44cee297f6d31
Michal Čihař <michal@cihar.com>
2017-01-19 19:51:56 +0100

    Update copyright notice
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- wlc/test_wlc.py
+++ wlc/test_wlc.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright © 2016 Michal Čihař <michal@cihar.com>
+# Copyright © 2016 - 2017 Michal Čihař <michal@cihar.com>
 #
 # This file is part of Weblate Client <https://github.com/WeblateOrg/wlc>
 #
