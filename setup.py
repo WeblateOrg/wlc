706ce492c455fe01a8b45930f59bce62567ccda9
Michal Čihař <michal@cihar.com>
2020-07-18 20:44:42 +0200

    Use setup.cfg for setuptools metadata

--- setup.py
+++ setup.py
@@ -21,58 +21,7 @@
 
 from setuptools import setup
 
-VERSION = None
-with open("wlc/__init__.py") as handle:
-    for line in handle.readlines():
-        if line.startswith("__version__"):
-            VERSION = line.split("=")[1].strip().strip('"')
-            break
-
-with open("README.rst") as handle:
-    LONG_DESCRIPTION = handle.read()
-
 with open("requirements.txt") as handle:
     REQUIRES = handle.read().split()
 
-setup(
-    name="wlc",
-    version=VERSION,
-    author="Michal Čihař",
-    author_email="michal@cihar.com",
-    description=(
-        "A command line utility for Weblate, "
-        "translation tool with tight version control integration"
-    ),
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type="text/x-rst",
-    license="GPLv3+",
-    keywords="i18n l10n gettext git mercurial translate",
-    url="https://weblate.org/",
-    download_url="https://github.com/WeblateOrg/wlc",
-    project_urls={
-        "Issue Tracker": "https://github.com/WeblateOrg/wlc/issues",
-        "Documentation": "https://docs.weblate.org/",
-        "Source Code": "https://github.com/WeblateOrg/wlc",
-        "Twitter": "https://twitter.com/WeblateOrg",
-    },
-    platforms=["any"],
-    packages=["wlc"],
-    package_dir={"wlc": "wlc"},
-    install_requires=REQUIRES,
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Topic :: Software Development :: Internationalization",
-        "Topic :: Software Development :: Localization",
-        "Topic :: Utilities",
-    ],
-    entry_points={"console_scripts": ["wlc = wlc.main:main"]},
-    python_requires=">=3.6",
-)
+setup(install_requires=REQUIRES)
